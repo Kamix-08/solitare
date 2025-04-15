ff3e78c034cc7c47a64cfc971fd12fb64d66ad46
Kamix-08 <pawlowski.kaka@gmail.com>
2025-04-15 18:54:40 +0200

    basic structure + cards & piles class implementation

---
+++ game/cards.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+class Value(Enum):
+    ACE  = 1; TWO = 2;  THREE = 3;  FOUR  = 4
+    FIVE = 5; SIX = 6;  SEVEN = 7;  EIGHT = 8
+    NINE = 9; TEN = 10; JACK  = 11; QUEEN = 12
+    KING = 13
+
+    symbols = {
+        ACE: 'A',
+        JACK: 'J',
+        QUEEN: 'Q',
+        KING: 'K'
+    }
+
+    def __str__(self):
+        return self.symbols.get(self, str(self.value))
+
+class Suit(Enum):
+    SPADES = 0; CLUBS = 1
+    HEARTS = 2; DIAMONDS = 3
+
+    symbols = {
+        SPADES: "♠",
+        CLUBS: "♣",
+        HEARTS: "♥",
+        DIAMONDS: "♦"
+    }
+
+    def __str__(self):
+        return self.symbols[self]
+    
+    def is_red(self) -> bool:
+        return self.value == Suit.HEARTS or self.value == Suit.DIAMONDS
+    
+class Card:
+    def __init__(self, value:Value, suit:Suit):
+        self.value = value
+        self.suit = suit
+
+    def __str__(self):
+        return f"{self.value}{self.suit}"
