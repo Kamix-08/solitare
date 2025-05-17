8d5abb8786ab3c8628bff8d5135b5f872ee2032b
Kamix-08 <pawlowski.kaka@gmail.com>
2025-05-17 11:07:17 +0200

    add leaderboard

--- game/scenes.py
+++ game/scenes.py
@@ -14,6 +14,7 @@ def start(mode:bool) -> None:
     gameManager.menu.start()
 
     use_scene(scene_end(
+        mode,
         gameManager.won, 
         gameManager.moves,
         time.time() - gameManager.time
@@ -36,9 +37,10 @@ def scene_main() -> Scene:
         update=lambda: print(scene), 
         highlight='green', 
         text=[
-            ('New Game', lambda: use_scene(scene_game_mode())),
-            ('Info',     lambda: use_scene(scene_info())),
-            ('\nExit',   lambda: exit_game())
+            ('New Game',    lambda: use_scene(scene_game_mode())),
+            ('Leaderboard', lambda: use_scene(scene_leaderboard())),
+            ('Info',        lambda: use_scene(scene_info())),
+            ('\nExit',      lambda: exit_game())
         ]
     )
 
@@ -82,10 +84,11 @@ def scene_info() -> Scene:
     scene += menu
     return scene
 
-def scene_end(won:bool, moves:int, time:float) -> Scene:
+def scene_end(mode:bool, won:bool, moves:int, time:float) -> Scene:
     scene:Scene = Scene()
     scene += AsciiText("You won!" if won else "You lost...", 'blue' if won else 'red')
 
+    scene += f"Mode : {"easy" if mode else "hard"}"
     scene += f"Moves: {moves}"
     scene += f"Time : {Renderer.format_seconds(time)}"
 
@@ -94,10 +97,83 @@ def scene_end(won:bool, moves:int, time:float) -> Scene:
         highlight='green',
         text=[
             ('Play Again',  lambda: use_scene(scene_game_mode())),
+            ('Leaderboard', lambda: use_scene(scene_leaderboard(mode))),
             ('Main Menu',   lambda: use_scene(scene_main())),
             ('\nExit',      lambda: exit_game())
         ]
     )
+    
+    if won:
+        with open('leaderboard.txt', 'a') as f:
+            f.write(f"{'e' if mode else 'h'}{moves}\n")
 
+    scene += menu
+    return scene
+
+def format_table(data:list[list[str]], headers:list[str]) -> list[str]:
+    print(data, headers)
+    assert all([len(x) == len(headers) for x in data])
+    
+    def get_line(elements:list[str]) -> str:
+        return ' ' + ' | '.join(elements) + ' '
+    
+    def center(element:str, width:int) -> str:
+        n:int = (width - len(element)) // 2
+        return ' ' * n + element + ' ' * (width - n - len(element))
+    
+    widths:list[int] = [max([len(headers[i])] + [len(x[i]) for x in data]) for i in range(len(headers))]
+    
+    res:list[str] = [get_line([center(x, widths[i]) for i, x in enumerate(line)]) for line in [headers] + data]
+    
+    for i in range(len(res) - 1):
+        res[i] += f"\n{'-' * len(res[i])}"
+    
+    return res
+
+def scene_leaderboard(mode:bool|None = None) -> Scene:
+    scene:Scene = Scene()
+    scene += AsciiText("Leaderboard", 'blue')
+    
+    flag:str = 'e' if mode else 'h'
+    
+    with open('leaderboard.txt', 'r') as f:
+        entries:list[str] = f.readlines()
+    
+    data:tuple[list[int],list[int]] = ([],[])
+    
+    def parse_entry(entry:str) -> tuple[int,bool]:
+        return int(entry[1:]), entry[0] == 'h'
+    
+    for entry in entries:
+        if mode is not None and entry[0] != flag:
+            continue
+        
+        v, _i = parse_entry(entry)
+        data[_i].append(v)
+        
+    for x in data:
+        x.sort()
+    
+    table:list[list[str]] = []
+    for i in range(max([len(x) for x in data])):
+        line:list[str] = [f"{i+1}."]
+        
+        for x in data:
+            if len(x) == 0 and mode is not None: continue
+            line.append(str(x[i]) if i < len(x) else '-')
+        
+        table.append(line)
+        
+    for _line in format_table(table, ["#"] + (["Easy", "Hard"] if mode is None else ["Moves"])):
+        scene += _line
+        
+    menu:Menu = Menu(
+        update=lambda: print(scene),
+        highlight='green',
+        text=[
+            ('Back', lambda: use_scene(scene_main()))
+        ]
+    )
+    
     scene += menu
     return scene
